4d4d3200ff9546f1176f56f356835ddaf7a608eb
gab <givanes@pm.me>
2024-06-05 14:40:51 +0200

    init

---
+++ greet/greet_server/server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"greetpb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", listener)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
---
+++ magefiles/magefile.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	// mg contains helpful utility functions, like Deps
+)
+
+// Build Protobuf files
+func Pb() error {
+	//mg.Deps(Clean)
+	fmt.Println("Building protobuf files...")
+	_, err := exec.Command("protoc", "--go_out=greet/greetpb", "greet/greetpb/greet.proto").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
+
+}
+
+// Clean Protobuf files
+func Clean() error {
+	//mg.Deps(setopt)
+	fmt.Println("Removing generated protobuf files...")
+	_, err := exec.Command("rm", "-f", "greet/greetpb/greet.pb.go").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
+
+// Removing files suppression confirmation
+func setopt() error {
+	_, err := exec.Command("setopt", "rmstarsilent").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
