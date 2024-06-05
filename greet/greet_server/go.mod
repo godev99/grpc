5cfd539e8f19cfb0b1e26298e8e0eca3342016b2
gab <givanes@pm.me>
2024-06-05 15:54:50 +0200

    protoc generation

--- greet/greet_server/go.mod
+++ greet/greet_server/go.mod
@@ -5,6 +5,7 @@ go 1.22.4
 require google.golang.org/grpc v1.64.0
 
 require (
+	github.com/godev99/grpc v0.0.0-20240605133340-5044117b686f // indirect
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -11,7 +11,7 @@ import (
 func Pb() error {
 	//mg.Deps(Clean)
 	fmt.Println("Building protobuf files...")
-	_, err := exec.Command("protoc", "--go_out=greet", "greet/greetpb/greet.proto").Output()
+	_, err := exec.Command("protoc", "--go_out=.", "--go-grpc_out=.", "--go_opt=paths=source_relative", "--go-grpc_opt=paths=source_relative", "greet/greetpb/greet.proto").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,6 +24,10 @@ func Clean() error {
 	//mg.Deps(setopt)
 	fmt.Println("Removing generated protobuf files...")
 	_, err := exec.Command("rm", "-f", "greet/greetpb/greet.pb.go").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = exec.Command("rm", "-f", "greet/greetpb/greet_grpc.pb.go").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
